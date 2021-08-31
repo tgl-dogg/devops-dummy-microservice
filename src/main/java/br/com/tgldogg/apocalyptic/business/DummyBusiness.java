9068b89932d3f5a5ecfd4260a6869bcd9ea01d26
[TGL] Dogg <vinidoggll@hotmail.com>
2021-08-31 15:28:30 -0300

    fixing package and adding sample method

---
+++ src/main/java/br/com/tgldogg/apocalyptic/business/DummyBusiness.java
@@ -0,0 +1,49 @@
+package br.com.tgldogg.apocalyptic.business;
+
+public class DummyBusiness {
+
+    private static final int NUMBER_OF_CHARACTERS = 256;
+    private static final int INPUT_MAX_SIZE = 1_000_000;
+
+    public boolean isStringPalindromeAnagram(final String str) {
+        checkInputSize(str);
+        checkInputIsMadeOfLetters(str);
+
+        final int[] count = new int[NUMBER_OF_CHARACTERS];
+
+        // For each character in input strings, increment count in the corresponding
+        // count array
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i)]++;
+
+        // Count odd occurring characters, there can be a maximum of one.
+        int odds = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (isOdd(count(i)))
+                odds++;
+
+            if (odds > 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    private void checkInputSize(final String str) {
+        if (str.length() > INPUT_MAX_SIZE) {
+            throw new Exception("Input size must be lower than " + INPUT_MAX_SIZE);
+        }
+    }
+
+    private void checkInputIsMadeOfLetters(final String str) {
+        final boolean allLetters = str.chars().allMatch(Character::isLetter);
+
+        if (!allLetters) {
+            throw new Exception("Input must be formed by letters.");
+        }
+    }
+
+    private boolean isOdd(final int number) {
+        return (number & 1) != 0;
+    }
+}
--- src/test/java/br/com/tgldogg/apocalyptic/ApocalypticEndWarApplicationTests.java
+++ src/test/java/br/com/tgldogg/apocalyptic/ApocalypticEndWarApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.tgldogg.Apocalyptic;
+package br.com.tgldogg.apocalyptic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
